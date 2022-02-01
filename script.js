95bee25be37dd047a1d5abc73b8f97a3e01f5ac7
usorfaitheloho <usorfaitheloho@gmail.com>
2022-02-01 10:54:18 -0800

    modify style.css

--- script.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const navWrapper = document.querySelector("desktop_nav_wrapper");
-
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navWrapper.classList.toggle("active");
-})
