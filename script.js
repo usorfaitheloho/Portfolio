bd183186af0581bf379755a840d83b39d5eade5e
usorfaitheloho <usorfaitheloho@gmail.com>
2022-02-01 13:53:38 -0800

    Fix linter error

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
-
-let menuLinks = document.querySelectorAll('.menu_links');
-let container = document.getElementsByClassName('toggle-menu')[0];
-let icon =document.querySelector(".hamburger .fa-bars");
-let logo = document.querySelector('.logo');
+const container = document.getElementsByClassName('toggle-menu')[0];
+const icon = document.querySelector('.hamburger .fa-bars');
+const logo = document.querySelector('.logo');
 
 container.style.visibility = 'hidden';
 
-
 function changeIcon() {
-    icon.classList.toggle("fa-times");
-    icon.classList.toggle("fa-bars");
-    icon.style.position = 'absolute';
-    logo.style.visibility = 'hidden';
-    icon.style.right ='0';
-    icon.style.marginRight = '24px';
-    if(container.style.visibility === 'hidden'){
-        container.style.visibility = 'visible';
-    }
- else if (container.style.visibility = 'hidden'){
-        container.style.visibility = 'hidden';
-        logo.style.visibility = 'visible';
-     }
-    
+  icon.classList.toggle('fa-times');
+  icon.classList.toggle('fa-bars');
+  icon.style.position = 'absolute';
+  logo.style.visibility = 'hidden';
+  icon.style.right = '0';
+  icon.style.marginRight = '24px';
+  if (container.style.visibility === 'hidden') {
+    container.style.visibility = 'visible';
+  } else if (container.style.visibility !== 'hidden') {
+    container.style.visibility = 'hidden';
+    logo.style.visibility = 'visible';
+  }
 }
 
-function hideMenu (){
-    container.style.visibility = 'hidden ' ;
-    logo.style.visibility = 'visible';
-    icon.classList.toggle("fa-times");
-    icon.classList.toggle('fa-bars');
+function hideMenu() {
+  container.style.visibility = 'hidden ';
+  logo.style.visibility = 'visible';
+  icon.classList.toggle('fa-times');
+  icon.classList.toggle('fa-bars');
 }
+changeIcon();
+hideMenu();
