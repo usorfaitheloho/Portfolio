5dc21f3f5e45de20ee8fb99cef75ae1f4f364718
usorfaitheloho <usorfaitheloho@gmail.com>
2022-10-18 12:24:55 -0700

    Add an extra file to seperate datas

---
+++ projectDatas.js
@@ -0,0 +1,67 @@
+
+// Project section dynamic load and pop-up window implementation
+
+const projectDatas = [
+    {
+      projectTitle: 'covid-19-data-app',
+      description: 'This is a react project that displays covid details for different countries and regions within those regions.',
+      featuredImage: './images/Covid-app2.PNG',
+      technologies: ['html', 'css', 'javascript'],
+      liveVersionImage: ['./images/seeLiveIcon.png'],
+      sourceImage: ['./images/Vector.png'],
+      liveVersion: 'https://covid-19-data-app.netlify.app/',
+      source: 'https://github.com/usorfaitheloho/covid-19-data-app',
+    },
+    {
+      projectTitle: 'NetMovie',
+      description: 'NetMovie is a web application based on an external TV Maze API that provides details about movies, showing comment section and reservations as well as booking. The API provides data about TV Shows',
+      featuredImage: './images/Net-movies.PNG',
+      technologies: ['html', 'css', 'javascript'],
+      liveVersionImage: ['./images/seeLiveIcon.png'],
+      sourceImage: ['./images/Vector.png'],
+      liveVersion: 'https://usorfaitheloho.github.io/NetMovie/dist/',
+      source: 'https://github.com/usorfaitheloho/NetMovie',
+    },
+    {
+      projectTitle: 'Space Travelers Hub',
+      description: 'Space Travelers Hub is a web application made for a company that provides commercial and scientific space travel services. The application allows users (future clients) to book rockets and join selected space missions.',
+      featuredImage: './images/space-travellers.PNG',
+      technologies: ['html', 'css', 'javascript'],
+      liveVersionImage: ['./images/seeLiveIcon.png'],
+      sourceImage: ['./images/Vector.png'],
+      liveVersion: 'sbf-spacetravellers.herokuapp.com/',
+      source: 'https://github.com/usorfaitheloho/space-travellers',
+    },
+    {
+      projectTitle: 'Book-store',
+      description: 'This is an application for users. Where each user can add or delete books and their authors',
+      featuredImage: './images/Book-Store.PNG',
+      technologies: ['html', 'css', 'javascript'],
+      liveVersionImage: ['./images/seeLiveIcon.png'],
+      sourceImage: ['./images/Vector.png'],
+      liveVersion: 'https://usorfaitheloho-book-store.netlify.app/',
+      source: 'https://github.com/usorfaitheloho/book-store',
+    },
+    {
+      projectTitle: 'Doctors Appointment',
+      description: 'Doctor appointment app is a software application that is especially meant to help patients manage and schedule appointments with the doctors or opt for a real-time chat.',
+      featuredImage: './images/doctors-app.PNG',
+      technologies: ['html', 'css', 'javascript'],
+      liveVersionImage: ['./images/seeLiveIcon.png'],
+      sourceImage: ['./images/Vector.png'],
+      liveVersion: 'https://alwayscare.herokuapp.com/doctors',
+      source: 'https://github.com/vickymarz/doctors-appointment-frontend',
+    },
+    {
+      projectTitle: 'HackHerThon',
+      description: 'This is a website for a HackHerTon event I developed as part of my capstone project for the Microverse program module 1. The website is for an upcoming hack event for female Software developers , to be held in Nigeria.',
+      featuredImage: './images/Hackherthon.PNG',
+      technologies: ['html', 'css', 'javascript'],
+      liveVersionImage: ['./images/seeLiveIcon.png'],
+      sourceImage: ['./images/Vector.png'],
+      liveVersion: 'usorfaitheloho.github.io/HackHerThon/',
+      source: 'https://github.com/usorfaitheloho/HackHerThon',
+    },
+  ];
+
+  export default projectDatas;
